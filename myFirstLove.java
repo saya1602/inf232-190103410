cb68ac8803c93a50284d5857d77a9fe20a00b679
saya1602 <190103410@stu.sdu.edu.kz>
2021-02-06 20:53:19 +0500

    Some java code

---
+++ myFirstLove.java
@@ -0,0 +1,11 @@
+public class myFirstLove {
+    public static void main(String[] args) {
+        System.out.println("Java is the most beautiful language I've ever seen");
+        boolean youAreBoring = true;
+        if(youAreBoring) {
+            System.out.println("Code in Java");
+        } else {
+            System.out.println("Code in Java, heh");
+        }
+    }
+}
